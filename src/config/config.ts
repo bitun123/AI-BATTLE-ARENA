f5caa242ebe097fd4b2ea73cb1936628f8a2c49e
satyajit <satyajitdasmahapatra2002@gmail.com>
2026-04-04 14:49:59 +0530

    first commit

---
+++ src/config/config.ts
@@ -0,0 +1,15 @@
+import { MistralAI } from "@langchain/mistralai"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+
+
+
+ const config = {
+GOOGLE_API_KEY : process.env.GOOGLE_API_KEY || "",
+MISTRALAI_API_KEY : process.env.MISTRALAI_API_KEY || "",
+COHERE_API_KEY : process.env.COHERE_API_KEY || ""
+}
+
+export default config
